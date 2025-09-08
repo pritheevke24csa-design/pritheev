87367cda7f8667ec70ed1c5959f7864b2f2648e8
pritheevke24csa-design <pritheevke24csa@vetias.ac.in>
2025-09-08 13:42:24 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// Add event listener to nav links
+document.querySelectorAll('nav a').forEach((link) => {
+    link.addEventListener('click', (e) => {
+        e.preventDefault();
+        const targetId = link.getAttribute('href');
+        document.querySelector(targetId).scrollIntoView({ behavior: 'smooth' });
+    });
+});
